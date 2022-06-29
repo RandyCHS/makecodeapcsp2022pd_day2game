892ccd6f14b4ac0d0f78454cb8853b493ee3de72
Randy Macdonald <41399033+RandyCHS@users.noreply.github.com>
2022-06-29 09:35:26 -0700

    Added ability to destroy projectiles with effects (graphic & sound)

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    otherSprite.destroy(effects.confetti, 100)
+    music.baDing.play()
+})
 let projectile: Sprite = null
 let ForestBat = sprites.create(assets.image`ForestBat1`, SpriteKind.Player)
 scene.setBackgroundColor(7)
@@ -7,4 +12,5 @@ info.setScore(0)
 info.startCountdown(10)
 game.onUpdateInterval(500, function () {
     projectile = sprites.createProjectileFromSide(assets.image`ForestMushroom`, 50, 0)
+    projectile.setPosition(0, randint(0, 120))
 })
